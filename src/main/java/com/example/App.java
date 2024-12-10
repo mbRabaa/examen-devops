0a2eee06c926773903516e6e0212db8d628be654
mbRabaa <elmbarkirabea@gmail.com>
2024-12-10 10:11:34 +0000

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
+@RestController
+class GreetingController {
+
+    @GetMapping("/")
+    public String greet() {
+        return "Bonjour Master DevOps II";
+    }
+}
